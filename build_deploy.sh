180957069e2ce22de04770940292cd8c32a62737
Michael Skarbek <mskarbek@redhat.com>
2022-03-15 14:45:47 -0400

    Catch Nonetype error (#10)
    * tag latest image
    
    * catch nonetypes

--- build_deploy.sh
+++ build_deploy.sh
@@ -19,4 +19,8 @@ mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+
+docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
+docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
+
 docker --config="$DOCKER_CONF" logout
